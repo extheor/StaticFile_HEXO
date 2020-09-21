0b9b5fe9175a5858626583fb70555a5f5176f571
MaXiaoYu <57162041+extheor@users.noreply.github.com>
2020-09-22 00:56:58 +0800

    Update myTool.js

--- myTool.js
+++ myTool.js
@@ -8,3 +8,4 @@ xiaokang.imgList = [
   "https://cdn.jsdelivr.net/gh/extheor/images/%E5%8A%A8%E6%BC%AB%E5%9B%BE%E5%BA%93/lb4.jpg",
 ];
 xiaokang.randomBg(); // 调用随机背景的方法
+xiaokang.meting("2802022828", "netease", "playlist");
