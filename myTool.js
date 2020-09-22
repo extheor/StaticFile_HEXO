e25242978077065ce0d8c8c2eb736ed6e05d6dd4
MaXiaoYu <57162041+extheor@users.noreply.github.com>
2020-09-22 10:10:13 +0800

    Update myTool.js

--- myTool.js
+++ myTool.js
@@ -1,11 +1,11 @@
 var xiaokang = new xkTool(
-  //   "https://cdn.jsdelivr.net/gh/extheor/images/%E5%8A%A8%E6%BC%AB%E5%9B%BE%E5%BA%93/lb4.jpg"
-  "transparent"
+    "https://cdn.jsdelivr.net/gh/extheor/images/%E5%8A%A8%E6%BC%AB%E5%9B%BE%E5%BA%93/lb4.jpg"
+    // "transparent"
 );
 // xiaokang.magicCirle();
 xiaokang.footFish();
-xiaokang.imgList = [
-  "https://cdn.jsdelivr.net/gh/extheor/images/%E5%8A%A8%E6%BC%AB%E5%9B%BE%E5%BA%93/lb4.jpg",
-];
-xiaokang.randomBg(); // 调用随机背景的方法
+// xiaokang.imgList = [
+//   "https://cdn.jsdelivr.net/gh/extheor/images/%E5%8A%A8%E6%BC%AB%E5%9B%BE%E5%BA%93/lb4.jpg",
+// ];
+// xiaokang.randomBg(); // 调用随机背景的方法
 xiaokang.meting("2802022828", "netease", "playlist");
